4a57603c3b36fffb9b1aae74ea3edaea56a37c5b
unknown <tobiburrohman2@gmail.com>
2022-07-26 11:18:11 +0600

    update make admin api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -24,11 +24,55 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// jwt verify/check function
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     await client.connect();
     const BlogCollections = client.db("flexiblog").collection("blogs");
     const UserCollections = client.db("flexiblog").collection("users");
+
+    // verify admin function
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await UserCollections.findOne({
+        email: requester,
+      });
+
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "Forbidden" });
+      }
+    };
+
+    // make admin API
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await UserCollections.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // get all blogs load
     app.get("/blogs", async (req, res) => {
@@ -37,25 +81,64 @@ async function run() {
     });
 
     // get all users load
-    app.get("/users", async (req, res) => {
+    app.get("/users",  async (req, res) => {
       const result = await UserCollections.find().toArray();
       res.send(result);
     });
 
+    // step 1 => update one by one user specific by email..
+    // step 2 => save registered user in db
+    // step 3 => after completing all above this, giving every user a jwt token..
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const user = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+
+      const result = await UserCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      // giving every user a jwt token
+      const token = jwt.sign({ email: email }, process.env.JWT_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ result, accessToken: token });
+    });
+
     // get just one blog  load by _id..get valid _id
     app.get("/blog/:id([0-9a-fA-F]{24})", async (req, res) => {
-        const id = req.params.id;
-        const query = {_id:ObjectId(id)};
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
       const result = await BlogCollections.findOne(query);
       res.send(result);
     });
 
-    app.post('/login', async(req, res)=>{
+    // post server side validation for new blogs
+    app.post("/blogs", async (req, res) => {
+      const blog = req.body;
+      // console.log('blog', blog)
+      const result = await BlogCollections.insertOne(blog);
+      res.send(result);
+    });
+
+    // delete blogs with _id
+    app.delete("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await BlogCollections.deleteOne(filter);
+      res.send(result);
+    });
+
+    app.post("/login", async (req, res) => {
       const user = req.body;
-      console.log('user', user)
-    })
-
-
+      console.log("user", user);
+    });
   } finally {
     //   await client.close();
   }
